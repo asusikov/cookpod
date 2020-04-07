e7e439c554d6fb58b7cda810b8beac655d301e83
Alexander Susikov <susikov.alexander@gmail.com>
2020-04-08 02:11:31 +0300

    Added dialyxir for testing

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule Cookpod.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.3", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test}
     ]
   end

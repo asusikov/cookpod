72dc1e92108f2e5f22a37599857071cca1c975e6
Alexander Susikov <susikov.alexander@gmail.com>
2020-04-08 01:13:39 +0300

    Moved warnings_as_errors to CI

--- mix.exs
+++ mix.exs
@@ -10,7 +10,6 @@ defmodule Cookpod.MixProject do
       version: "0.1.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
-      elixirc_options: [warnings_as_errors: true],
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),

fe0a6440de723a8f32ae1fa0e4bc1ad5d665379f
Alexander Susikov <susikov.alexander@gmail.com>
2020-04-08 01:30:27 +0300

    Formatted text

--- mix.exs
+++ mix.exs
@@ -15,7 +15,12 @@ defmodule Cookpod.MixProject do
       aliases: aliases(),
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-    preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 

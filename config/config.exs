3ef1ac506a0ccd350aa6831a55d0e39aff119ac0
asusikov <susikov.alexander@gmail.com>
2020-05-08 01:00:35 +0300

    Added translation of terms

--- config/config.exs
+++ config/config.exs
@@ -31,6 +31,8 @@ config :phoenix, :template_engines,
   slime: PhoenixSlime.Engine,
   slimleex: PhoenixSlime.LiveViewEngine # If you want to use LiveView
 
+config :cookpod, CookpodWeb.Gettext, locales: ["en", "ru"], default_locale: "ru"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"

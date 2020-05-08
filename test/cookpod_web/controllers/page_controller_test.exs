24970b1861e50f0782d3bbbce3db3beb17cfcd18
asusikov <susikov.alexander@gmail.com>
2020-05-08 09:46:31 +0300

    Fixed tests

--- test/cookpod_web/controllers/page_controller_test.exs
+++ test/cookpod_web/controllers/page_controller_test.exs
@@ -1,8 +1,9 @@
 defmodule CookpodWeb.PageControllerTest do
   use CookpodWeb.ConnCase
+  import CookpodWeb.Gettext
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ gettext("Welcome to %{name}!", name: "Phoenix")
   end
 end
